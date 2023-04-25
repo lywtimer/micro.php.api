ef32b40c2db1bc8a792be11a35946daa881e703f
好好住-liyuanwu <liyuanwu@haohaozhu.com>
2023-04-25 19:04:53 +0800

    apply

--- cmd/index.php
+++ cmd/index.php
@@ -1,5 +1,7 @@
 <?php
 
+use mszl\core\middleware\AbstractMiddleware;
+use mszl\core\middleware\MiddlewareStack;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
@@ -21,10 +23,46 @@ elseif (preg_match('/\.(?:service)$/', $path))
     $server = (new Yar_Server(new Operator()))->handle();
 else {
 //    echo "<p>Welcome to PHP</p>";
-    \mszlu\memory\Engine::getInstance("")->run();
-    $response = new Response();
-    $response->setStatusCode(Response::HTTP_OK);
-    $response->headers->set("Content-Type", "application/json;charset=utf-8");
-    $response->setContent(json_encode(["hello" => "小鬼", "msg" => "<p>Welcome to PHP</p>"]));
-    $response->send();
+    class TimeMiddleware extends AbstractMiddleware
+    {
+        public function handle($request, MiddlewareStack $stack)
+        {
+            $start = microtime(true);
+
+            $response = $stack->next($request);
+
+            $end = microtime(true);
+            $time = $end - $start;
+
+            echo sprintf('Request time: %s sec' . PHP_EOL, $time);
+
+            return $response;
+        }
+
+    }
+
+
+
+    class OrderMiddleware extends AbstractMiddleware
+    {
+
+        public function handle($request, MiddlewareStack $stack)
+        {
+            // TODO: Implement handle() method.
+            echo "我处理了订单业务", PHP_EOL;
+            return $stack->next($request);
+        }
+    }
+
+    $middlewares = [
+        new TimeMiddleware(),
+        new OrderMiddleware(),
+        new ErrorHandlerMiddleware()
+    ];
+    \mszl\core\Engine::getInstance("")->addMiddleware(...$middlewares)->addMiddleware(new OrderMiddleware())->run();
+//    $response = new Response();
+//    $response->setStatusCode(Response::HTTP_OK);
+//    $response->headers->set("Content-Type", "application/json;charset=utf-8");
+//    $response->setContent(json_encode(["hello" => "小鬼", "msg" => "<p>Welcome to PHP</p>"]));
+//    $response->send();
 }
